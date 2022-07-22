ac76a2a48cf9ecbada309be9c4ce974fe01dca7e
言亥歹匕 <qinguangcan@vip.qq.com>
2022-07-22 12:24:56 +0800

    更新frp客户端到0.44.0版本

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "内网穿透利器，谁用谁知道。", 
     "home_url": "Module_frpc.asp", 
-    "md5": "57F2207FE01FE4BB680F8891EAED0480", 
+    "md5": "BB9CD5BDF9F67EA223F410E770D7A62B", 
     "module": "frpc", 
     "title": "frpc穿透", 
     "version": "3.0.0"
